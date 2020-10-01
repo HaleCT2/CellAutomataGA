c1657845c66d74c6db57a59b7eaa6761a669a692
HaleCT2 <halect2@miamioh.edu>
2020-10-01 12:32:59 -0400

    Add Population Support

--- Golly Scripts/golly-script-test.py
+++ Golly Scripts/golly-script-test.py
@@ -1,7 +1,7 @@
 # File:         golly-script-test.py
 # Author:       Carter Hale
 # Date Created: September 9, 2020
-# Last Updated: September 22, 2020
+# Last Updated: October 1, 2020
 
 # Golly doesn't offer standalone Library so the Module is instantiated
 # when the Executable is launched. Script can only be ran from within Golly.
@@ -57,7 +57,7 @@ def write_bmp(bmp_fileName):
     wd = prect[2]
     ht = prect[3]
 
-    colors = g.getcolors()     # White & Black
+    colors = g.getcolors()
     state0 = (colors[1], colors[2], colors[3])
 
     # Create 2D Array of Pixels
@@ -89,72 +89,82 @@ def compare_rle(fileName1, fileName2):
 # -----------------------------------------------------------------------------
 
 
-# Create New Window and fill X% of YxY Square Grid with Random Noise
-g.new("test-pattern")
+# Ask User Configuration Questions
 gridSize = g.getstring("Enter Side Length of Grid:", "100", "Grid Size")
 fillPerc = g.getstring("Enter Grid Fill Percentage:", "50", "Fill Percentage")
-g.select([0, 0, int(gridSize), int(gridSize)])
-g.randfill(int(fillPerc))
 
-# Declare Algorithm and Rule
-g.setalgo("QuickLife")
 customRule = g.getstring("If you wish to use a Specific Rule,\n" +
                          "enter it below with 'B0...8/S0...8' Notation.\n" +
                          "Otherwise, press 'OK' for Random Generation:",
                          "Random", "Specific Rule Set")
 
-if (customRule == "Random"):
-    rule = make_rule()
-else:
-    rule = customRule.lower()
+numPopulation = g.getstring("Enter Population Size of Individual CA Rules:\n" +
+                            "(Note: Does not work with Specific Rule)",
+                            "1", "Population Size")
 
-g.setrule(rule)
-
-# Creates "Test Patterns" Folder within Golly Directory if it does not Exist
-fileLoc = g.getdir("app") + "Test Patterns\\"
-if (os.path.isdir(fileLoc) is not True):
-    os.mkdir(fileLoc)
-
-# Creates Subfolder specific to Rule Set to hold Generation Patterns
-fileLoc += rule.replace("/", "_") + "\\"
-if (os.path.isdir(fileLoc) is not True):
-    os.mkdir(fileLoc)
-# Prepare File Names for each Genereration's Pattern File
-fileNamePrefix = fileLoc + rule.replace("/", "_") + "_"
-
-# Go through N Generations and save Pattern at each
-numGenerations = g.getstring("Enter Number of Generations to Explore:",
+numGenerations = g.getstring("Enter Number of CA Generations to Explore\n" +
+                             "for Each Individual Rule in the Population:",
                              "10", "Number of Generations")
 
-# Choose File Format
 fileChoice = g.getstring("Enter Preferred Pattern File Format\n" +
                          "(Either 'RLE' or 'BMP'):", "RLE", "File Format")
 
-for i in range(int(numGenerations) + 1):
-    # Stop Loop if Universe is Empty
-    if (g.empty()):
-        break
+# Loop Process for Each Individual Rule in the Population
+for j in range(int(numPopulation)):
+    # Create New Window and Fill X% of YxY Square Grid with Random Noise
+    g.new("test-pattern")
+    g.select([0, 0, int(gridSize), int(gridSize)])
+    g.randfill(int(fillPerc))
 
-    # Determine File Names
-    fileNameRLE = fileNamePrefix + str(i) + ".rle"
-    fileNameBMP = fileNamePrefix + str(i) + ".bmp"
-    # Determine Previous File Names
-    fileNamePrevRLE = fileNamePrefix + str(i-1) + ".rle"
-    fileNamePrevBMP = fileNamePrefix + str(i-1) + ".bmp"
+    # Declare Algorithm and Rule
+    g.setalgo("QuickLife")
 
-    if (fileChoice == "RLE"):
-        g.save(fileNameRLE, "rle")
-        # Compare Previous Generation to Determine Class I Systems
-        if (i > 0 and compare_rle(fileNameRLE, fileNamePrevRLE)):
+    if (customRule == "Random"):
+        rule = make_rule()
+    else:
+        rule = customRule.lower()
+
+    g.setrule(rule)
+
+    # Creates "Test Patterns" Folder within Directory if it does not Exist
+    fileLoc = g.getdir("app") + "Test Patterns\\"
+    if (os.path.isdir(fileLoc) is not True):
+        os.mkdir(fileLoc)
+
+    # Creates Subfolder specific to Rule Set to hold Generation Patterns
+    fileLoc += rule.replace("/", "_") + "\\"
+    if (os.path.isdir(fileLoc) is not True):
+        os.mkdir(fileLoc)
+
+    # Prepare File Names for each Genereration's Pattern File
+    fileNamePrefix = fileLoc + rule.replace("/", "_") + "_"
+
+    # Loop and Save Patterns
+    for i in range(int(numGenerations) + 1):
+        # Stop Loop if Universe is Empty
+        if (g.empty()):
             break
 
-    elif (fileChoice == "BMP" or fileChoice == "bmp"):
-        write_bmp(fileNameBMP)
-        # Compare Previous Generation to Determine Class I Systems
-        if (i > 0 and filecmp.cmp(fileNameBMP, fileNamePrevBMP)):
-            break
+        # Determine File Names
+        fileNameRLE = fileNamePrefix + str(i) + ".rle"
+        fileNameBMP = fileNamePrefix + str(i) + ".bmp"
+        # Determine Previous File Names
+        fileNamePrevRLE = fileNamePrefix + str(i-1) + ".rle"
+        fileNamePrevBMP = fileNamePrefix + str(i-1) + ".bmp"
 
-    g.run(1)
+        if (fileChoice == "RLE"):
+            g.save(fileNameRLE, "rle")
+            # Compare Previous Generation to Determine Class I Systems
+            if (i > 0 and compare_rle(fileNameRLE, fileNamePrevRLE)):
+                break
+
+        elif (fileChoice == "BMP" or fileChoice == "bmp"):
+            write_bmp(fileNameBMP)
+            # Compare Previous Generation to Determine Class I Systems
+            if (i > 0 and filecmp.cmp(fileNameBMP, fileNamePrevBMP)):
+                break
+
+        g.run(1)
 
 # Prepare for Viewing
 g.fit()
