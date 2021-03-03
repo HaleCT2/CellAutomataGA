cbe8daca84250334015f8d17b36a488cfdce07cd
EricSchonauer <44148240+EricSchonauer@users.noreply.github.com>
2021-03-03 14:52:34 -0500

    Add files via upload

---
+++ ConwayClassifier/3_3_21/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: Eric Schonauer
+ *
+ * Created on September 18, 2020, 1:23 PM
+ */
+
+#include "ConwayClassifier.h"
+
+/*
+ * To test the ConwayClassifier class
+ */
+int main(int argc, char** argv) {
+    ConwayClassifier c("/home/schonaem/NetBeansProjects/"
+    "conwayClassifier/TestPatterns/b25_s26", 1000, 50);
+    //c.printGameBoard(1000, std::cout, '@', '.');
+    // now compare results to rle's in golly
+    std::cout << c.classification();
+    return 0;
+}
+
