fce5a63ad24914a4da41b431943b06b820b8d2a3
Carter <halect2@miamioh.edu>
2021-03-24 19:19:57 -0400

    Update TestSystem with new CC and Test Suite

---
+++ TestSystem/calcSpecRule.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "ConwayClassifier.h"
+
+
+/*
+ * To test certain Ruleset Simulations to obtain Metrics
+ */
+int main(int argc, char **argv)
+{   
+    // Variables
+    int gens;
+    std::string rule;
+   
+    // Get Inputs
+    std::cout << "Ruleset (e.g. b2_s23):";
+    std::cin >> rule;
+    std::cout << "Generation Count:";
+    std::cin >> gens;
+
+    // CC Constructor with 3 Threads and Calculating Back 30 Percent
+    ConwayClassifier c("/home/CellAutomataGA/Desktop/Golly Patterns/Test Patterns/" + rule, gens, 3, 30);
+
+    // Display Metrics
+    std::cout << std::endl << "PercentChangeAvg: " << c.getPercentChange() << std::endl;
+    std::cout << "AliveCellRatioAvg: " << c.getAliveCellRatio() << std::endl;
+    std::cout << "ActiveCellRatioAvg: " << c.getActiveCellRatio() << std::endl;
+    return 0;
+}
---
+++ TestSystem/experiment.py
@@ -0,0 +1,43 @@
+# File:         experiment.py
+# Author:       Carter Hale
+# Date Created: March 24, 2021
+# Last Updated: March 24, 2021
+
+# Element Tree for XML Parsing
+import xml.etree.ElementTree as ET
+# OS for System Calls to Run GA
+import os
+
+# THIS IS A SUPER BEGINNING, WE CAN INCORPORATE ALL THE METRICS AND WEIGHTS AND GET USER INPUT FOR
+# WHAT WE WANT TO LOOK AT AND WHERE WE WANNA INTERATE THROUGH
+
+# Retrieve Settings from XML
+tree = ET.parse('config.xml')
+root = tree.getroot()
+# Set Up XML Tree Root
+rootGA = root.find("GeneticAlgo")
+fitFun = rootGA.find("FitnessFunction")
+idealMet = fitFun.find("IdealMetrics")
+# First tests are for Active Metric
+active = idealMet.find("Active")
+activeMin = active.find("Min")
+activeMax = active.find("Max")
+# Set starting Metrics
+aMin = 0.0
+aMax = 0.05
+activeMin.text = str(aMin)
+activeMax.text = str(aMax)
+tree.write('config.xml')
+# Loop through Search Space
+for i in range(10):
+    # Run GA and save output to logs
+    os.system("unbuffer ./main.exe | tee " + str(i) + "test.log")
+    # Iterate over search space
+    activeMin = activeMin + .05
+    activeMax = activeMax + .05
+    # Update Config File
+    activeMin.text = str(aMin)
+    activeMax.text = str(aMax)
+    tree.write('config.xml')
+    # Remove Past Simulation Files
+    os.system("rm -r /home/CellAutomataGA/Desktop/Golly Patterns/Simulation")
