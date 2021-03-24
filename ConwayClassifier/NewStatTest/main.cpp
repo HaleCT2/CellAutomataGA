b17c4b588854f5b64418bdf5be71853b34a732b2
EricSchonauer <44148240+EricSchonauer@users.noreply.github.com>
2021-03-24 15:52:31 -0400

    Uploaded files that run on Miami Linux server, but throw a seg. fault on the VM

---
+++ ConwayClassifier/NewStatTest/main.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   main.cpp
+ * Author: Eric Schonauer
+ *
+ * Created on September 18, 2020, 1:23 PM
+ */
+
+#include "ConwayClassifier.h"
+
+/*
+ * To test the ConwayClassifier class
+ */
+int main(int argc, char** argv) {
+    int gens = 100;
+    int percent = 10;
+    int startGen = gens - (((double) gens / (double) 100) * (double) percent); 
+    ConwayClassifier c("/home/schonaem/NetBeansProjects/"
+    "conwayClassifier/TestPatterns/b25_s26", gens, 2, percent);
+    for (int i = startGen; i <= gens; i++) {
+        std::cout << "PercentChange " << i << ": " << c.getPercentChange(i) << std::endl;
+    }
+    std::cout << "PercentChangeAvg: " << c.getPercentChange() << std::endl;
+    for (int i = startGen; i <= gens; i++) {
+        std::cout << "AliveCellRatio " << i << ": " << c.getAliveCellRatio(i) << std::endl;
+    }
+    std::cout << "AliveCellRatioAvg: " << c.getAliveCellRatio() << std::endl;
+    for (int i =  startGen; i <= gens; i++) {
+        std::cout << "ActiveCellRatio: " << i << ": " << c.getActiveCellRatio(i) << std::endl;
+    }
+    std::cout << "ActiveCellRatioAvg: " << c.getActiveCellRatio() << std::endl;
+
+//    for (int i = startGen; i <= gens; i++) {
+//        std::cout << "ExpansionRateX: " << i << ": " << c.getExpansionRateX(i) << std::endl;
+//    }
+//    std::cout << "ExpansionRateXAvg: " << c.getExpansionRateX() << std::endl;
+//    for (int i = startGen; i <= gens; i++) {
+//        std::cout << "ExpansionRateY: " << i << ": " << c.getExpansionRateY(i) << std::endl;
+//    }
+//    std::cout << "ExpansionRateYAvg: " << c.getExpansionRateY() << std::endl;
+//    for (int i = startGen; i <= gens; i++) {
+//        std::cout << "ExpansionRateArea: " << i << ": " << c.getExpansionRateArea(i) << std::endl;
+//    }
+//    std::cout << "ExpansionRateAreaAvg: " << c.getExpansionRateArea() << std::endl;
+    return 0;
+}
+
