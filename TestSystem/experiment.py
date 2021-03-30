40c0d9061e72b580fb6dd40f6994c0d32d1fc751
Owen <hichenoe@miamioh.edu>
2021-03-29 21:55:49 -0400

    built out more of experiment.py script

--- TestSystem/experiment.py
+++ TestSystem/experiment.py
@@ -1,7 +1,7 @@
 # File:         experiment.py
-# Author:       Carter Hale
+# Author:       Carter Hale, Owen Hichens
 # Date Created: March 24, 2021
-# Last Updated: March 24, 2021
+# Last Updated: March 29, 2021
 
 # Element Tree for XML Parsing
 import xml.etree.ElementTree as ET
@@ -14,30 +14,49 @@ import os
 # Retrieve Settings from XML
 tree = ET.parse('config.xml')
 root = tree.getroot()
+
 # Set Up XML Tree Root
 rootGA = root.find("GeneticAlgo")
 fitFun = rootGA.find("FitnessFunction")
 idealMet = fitFun.find("IdealMetrics")
+
+# Get user input for changing variable and step size
+var = input("'Active', 'Percent', or 'Alive': ")
+minStep = float(input("Min step size: "))
+maxStep = float(input("Max step size: "))
+
 # First tests are for Active Metric
-active = idealMet.find("Active")
-activeMin = active.find("Min")
-activeMax = active.find("Max")
+changingVar = idealMet.find(var)
+changingMin = changingVar.find("Min")
+changingMax = changingVar.find("Max")
+
 # Set starting Metrics
-aMin = 0.0
-aMax = 0.05
-activeMin.text = str(aMin)
-activeMax.text = str(aMax)
+if var == 'Active':
+    cMin = 0.0
+    cMax = 0.05
+elif var == 'Percent':
+    cMin = 0.0
+    cMax = 0.4
+elif var == 'Alive':
+    cMin = 0.0
+    cMax = 0.49
+changingMin.text = str(cMin)
+changingMax.text = str(cMax)
 tree.write('config.xml')
+
 # Loop through Search Space
-for i in range(10):
+for i in range(3):
     # Run GA and save output to logs
-    os.system("./main.exe | tee " + str(i) + "test.log")
+    os.system("unbuffer ./main.exe | tee ./test_logs/" + str(i) + "test.log")
+
     # Iterate over search space
-    aMin = aMin + .05
-    aMax = aMax + .05
+    cMin = cMin + minStep
+    cMax = cMax + maxStep
+
     # Update Config File
-    activeMin.text = str(aMin)
-    activeMax.text = str(aMax)
+    changingMin.text = str(cMin)
+    changingMax.text = str(cMax)
     tree.write('config.xml')
+
     # Remove Past Simulation Files
     os.system("rm -r /home/CellAutomataGA/Desktop/Golly\ Patterns/Simulation")
